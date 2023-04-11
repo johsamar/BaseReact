e5cdb1178ff0cc6bee819f9f9174bad139094541
Alejandro Rios <alejandrorios2012@gmail.com>
2023-04-10 21:04:55 -0500

    Add new Component Alejandro to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Alejandro from "./components/alejandro";
 import Samuel from "./components/samuel";
 
 function App() {
   return (
     <div className="App">
       <Samuel />
+      {/* Este componente recibe un parametro "valor", el cual es un número que se usa para realizar algunas operaciones básicas */}
+      <Alejandro valor="10"/>
     </div>
   );
 }
