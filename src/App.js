2fe9927beabcaf05a5911e0e4d95807ece5b2051
alejoa27 <alejandro.agudelo2@utp.edu.co>
2023-04-11 10:16:12 -0500

    Crear Alejandro Agudelo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Samuel from "./components/samuel";
 import Cristian from "./components/Cristian";
+import AlejandroA from "./components/AlejandroA";
 
 function App() {
   return (
     <div className="App">
       <Samuel />
       <Cristian />
+      <AlejandroA />
     </div>
   );
 }
